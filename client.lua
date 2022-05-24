140232ecafbf70c3776f8373dc0b9787b194121a
Zotactiks <101468344+Zotactiks@users.noreply.github.com>
2022-05-24 04:16:53 +0100

    Context added
    (ON) Left Ally .Ect for notify

--- client.lua
+++ client.lua
@@ -34,10 +34,10 @@ AddEventHandler(
                     QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                 end
             else
-                QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
-            QBCore.Functions.Notify("You do not have access to the ELS Panel (This is not an emergency vehicle or you don't have access codes)", "error", 5000)
+            print('Local player is in a vehicle!')
         end
     end
 )
@@ -62,17 +62,17 @@ RegisterCommand(
                         if IsVehicleExtraTurnedOn(veh, extra) then
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
                             SetVehicleExtra(veh, extra, 1)
-                            QBCore.Functions.Notify("Removed", "error", 5000)
+                            QBCore.Functions.Notify("(OFF) Rear E-Lights", "error", 5000)
                         else
                             SetVehicleExtra(veh, extra, 0)
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
-                            QBCore.Functions.Notify("Added", "success", 5000)
+                            QBCore.Functions.Notify("(ON) Rear E-Lights", "success", 5000)
                         end
                     else
                         QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                     end
                 else
-                    QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                    QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
                 end
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
@@ -100,17 +100,17 @@ RegisterCommand(
                         if IsVehicleExtraTurnedOn(veh, extra) then
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
                             SetVehicleExtra(veh, extra, 1)
-                            QBCore.Functions.Notify("Removed", "error", 5000)
+                            QBCore.Functions.Notify("(OFF) Front E-Lights", "error", 5000)
                         else
                             SetVehicleExtra(veh, extra, 0)
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
-                            QBCore.Functions.Notify("Added", "success", 5000)
+                            QBCore.Functions.Notify("(ON) Front E-Lights", "success", 5000)
                         end
                     else
                         QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                     end
                 else
-                    QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                    QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
                 end
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
@@ -138,17 +138,17 @@ RegisterCommand(
                         if IsVehicleExtraTurnedOn(veh, extra) then
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
                             SetVehicleExtra(veh, extra, 1)
-                            QBCore.Functions.Notify("Removed", "error", 5000)
+                            QBCore.Functions.Notify("(OFF) R-L Traffic Advisory", "error", 5000)
                         else
                             SetVehicleExtra(veh, extra, 0)
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
-                            QBCore.Functions.Notify("Added", "success", 5000)
+                            QBCore.Functions.Notify("(ON) R-L Traffic Advisory", "success", 5000)
                         end
                     else
                         QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                     end
                 else
-                    QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                    QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
                 end
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
@@ -176,17 +176,17 @@ RegisterCommand(
                         if IsVehicleExtraTurnedOn(veh, extra) then
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
                             SetVehicleExtra(veh, extra, 1)
-                            QBCore.Functions.Notify("Removed", "error", 5000)
+                            QBCore.Functions.Notify("(OFF) L-R Traffic Advisory", "error", 5000)
                         else
                             SetVehicleExtra(veh, extra, 0)
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
-                            QBCore.Functions.Notify("Added", "success", 5000)
+                            QBCore.Functions.Notify("(ON) L-R Traffic Advisory", "success", 5000)
                         end
                     else
                         QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                     end
                 else
-                    QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                    QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
                 end
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
@@ -214,17 +214,17 @@ RegisterCommand(
                         if IsVehicleExtraTurnedOn(veh, extra) then
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
                             SetVehicleExtra(veh, extra, 1)
-                            QBCore.Functions.Notify("Removed", "error", 5000)
+                            QBCore.Functions.Notify("(OFF) Take Downs", "error", 5000)
                         else
                             SetVehicleExtra(veh, extra, 0)
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
-                            QBCore.Functions.Notify("Added", "success", 5000)
+                            QBCore.Functions.Notify("(ON) Take Downs", "success", 5000)
                         end
                     else
                         QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                     end
                 else
-                    QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                    QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
                 end
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
@@ -252,17 +252,17 @@ RegisterCommand(
                         if IsVehicleExtraTurnedOn(veh, extra) then
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
                             SetVehicleExtra(veh, extra, 1)
-                            QBCore.Functions.Notify("Removed", "error", 5000)
+                            QBCore.Functions.Notify("(OFF) Left Ally", "error", 5000)
                         else
                             SetVehicleExtra(veh, extra, 0)
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
-                            QBCore.Functions.Notify("Added", "success", 5000)
+                            QBCore.Functions.Notify("(ON) Left Ally", "success", 5000)
                         end
                     else
                         QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                     end
                 else
-                    QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                    QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
                 end
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
@@ -290,17 +290,17 @@ RegisterCommand(
                         if IsVehicleExtraTurnedOn(veh, extra) then
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
                             SetVehicleExtra(veh, extra, 1)
-                            QBCore.Functions.Notify("Removed", "error", 5000)
+                            QBCore.Functions.Notify("(OFF) Right Ally", "error", 5000)
                         else
                             SetVehicleExtra(veh, extra, 0)
                             TriggerServerEvent("InteractSound_SV:PlayOnSource", "on", 0.80)
-                            QBCore.Functions.Notify("Added", "success", 5000)
+                            QBCore.Functions.Notify("(ON) Right Ally", "success", 5000)
                         end
                     else
                         QBCore.Functions.Notify("This is not present on this vehicle", "error", 5000)
                     end
                 else
-                    QBCore.Functions.Notify("Your Vehicle is to damaged", "error", 5000)
+                    QBCore.Functions.Notify("Your Vehicle is too damaged", "error", 5000)
                 end
             end
         elseif IsPedInAnyVehicle(PlayerPedId(), true) then
